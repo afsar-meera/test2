2abc872bf74dca4513c9a0cdb676b25ff53eea05
Afsar <120561394+afsar-meera@users.noreply.github.com>
2023-12-22 12:20:20 +0530

    Add files via upload

---
+++ nlp.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from transformers import pipeline
+
+# Load the Hugging Face model for sentiment analysis
+sentiment_analyzer = pipeline("sentiment-analysis", model="bert-base-uncased")
+
+st.title("Sentiment Analysis with Hugging Face + Streamlit")
+
+# Create a text input for user input
+user_input = st.text_area("Enter text for sentiment analysis:")
+
+if st.button("Analyze Sentiment"):
+    # Analyze sentiment using the Hugging Face model
+    result = sentiment_analyzer(user_input)
+    sentiment = result[0]['label']
+    confidence = result[0]['score']
+
+    st.write(f"Sentiment: {sentiment}")
+    st.write(f"Confidence: {confidence}")
